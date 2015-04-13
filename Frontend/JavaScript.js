63f1a3b61251cd10bf7a7fe6093740adec472271
Oleg K <tinchest@live.com>
2015-04-12 19:49:06 -0700

    JS questions added

--- Frontend/JavaScript.js
+++ Frontend/JavaScript.js
@@ -16,6 +16,9 @@
  * @topic
  *      Language aspect that a question digs into.
  *
+ * @related
+ *     Related topics or questions (?)
+ *
  * @description
  *      Question definition, e.g what to ask when the related code is provided.
  *
@@ -29,7 +32,7 @@
  *
  * Misc.
  * 
- *  Place all related code inside a code block ({}) so it could be collapsed:
+ *  Place all code related to one question inside a code block ({}) so it could be collapsed:
  */
 
 
@@ -44,8 +47,8 @@
  * @topic: Property lookup uses prototype chain, assigning does not.
  * @description : What will be printed in console.
  * @answer: 
- *      console.info(bar.count) prints 1
- *      console.info(buz.count) prints 0
+ *      console.log(bar.count) prints 1
+ *      console.log(buz.count) prints 0
  *
  */
 {
@@ -74,8 +77,8 @@
         name: 'John'
     });
 
-    console.info(bar.count);
-    console.info(buz.count);
+    console.log(bar.count);
+    console.log(buz.count);
 }
 
 
@@ -85,7 +88,7 @@
  * @topic Prototype is a shared object.
  * @description What will be printed in console.
  * @additional How to change the code to make 'stats' calculate statistics for each instance independently.
- * @answer console.info(bar.stats.count) prints 2.
+ * @answer console.log(bar.stats.count) prints 2.
  * 
  */
 {
@@ -116,18 +119,270 @@
     bar.someFunction();
     buz.someFunction();
 
-    console.info(bar.stats.count);
+    console.log(bar.stats.count);
+}
+
+/**
+ * @number 3.
+ * @level Simple.
+ * @topic Closures
+ * @description What will be printed in console.
+ * @answer
+ *     console.log(operation1(3)) prints 9
+ *     console.log(operation2(2.5)) prints 5
+ * @additional What exactly happens with factor
+ */
+{
+    function getMultiplicator(factor) {
+        return function(val) {
+            return factor * val;
+        }
+    }
+
+    var operation1 = getMultiplicator(3);
+    var operation2 = getMultiplicator(2);
+
+    console.log(operation1(3));
+    console.log(operation2(2.5));
+
 }
 
 
 /**
+ * @number 4.
+ * @level Average/Advanced
+ * @topic Closures and variable scopes
+ * @description What will be printed in console.
+ * @answer
+ *     console.log(operation(2)) prints 0
+ *     console.log(operation(3)) prints 1
+ * 
+ */
+{
+
+    var count = 0;
+    var operation = (function(count) {
+        return function(value) {
+            console.log(count);
+            count++;
+            // some logic here
+        }
+
+    })(count);
+
+    console.log(operation(2));
+    console.log(operation(3))
+
+}
+
+
+
+
+/**
+ * @number 5.
+ * @level Simple
+ * @topic Closures/Execution context
+ * @description What will be printed in console.
+ * @answer
+ *     5 5 5 5 5
+ * @additional 
+ *     How to change this code to make it print numbers in order.
+ *     A: call set timeout in another cloure, or call 'bind'
+ *     
+ * 
+ */
+{
+    for (var i = 0; i < 5; i++) {
+        (function(realValue) {
+            setTimeout(function() {
+                console.log(realValue);
+            }, 0);
+        })(i);
+    }
+}
+
+// TODO: Advanced-level question about closures
+
+/**
+ * @number 6.
+ * @level Simple
+ * @topic Execution context
+ * @description What will be printed in console.
+ * @answer
+ *     7
+ * @additional 
+ *         Join ++ and return statements e.g. "return ++foo";
+ * 
+ *         Change func1 definition to:
+ * 
+ *             function func1() {
+ *                  ++foo;
+ *                  var foo = 0;
+ *                  return foo;
+ *             }
+ * 
+ *         Level Advanced
+ *         Answer 6, explanation: hoisting
+ * 
+ */
+{
+    var foo = 5;
+
+    function func1() {
+        var foo = 0;
+        ++foo;
+        return foo;
+    }
+
+    function func2() {
+        ++foo;
+        return foo;
+    }
+
+    console.log(func2() + func1());
+}
+
+
+/**
+ * @number 7.
+ * @level Average
+ * @topic Execution context / this keyword
+ * @description What will be printed in console.
+ * @answer 0.
+ * @additional
+ *     How to change the code so it calculates count correctly.
+ */
+{
+    var count = 5;
+
+    function Foo() {
+        this.count = 0;
+        this.addCount = function() {
+            count++;
+        };
+    }
+
+    var bar = new Foo();
+    bar.addCount();
+
+    console.log(bar.count);
+}
+
+/**
+ * @number 8.
+ * @level Simple
+ * @topic Tricks / Execution context / call built-in function
+ * @description Explain what happens in this code block
+ * @additional
+ *     Ask about apply and bind function.
+ * @answer
+ *     Anonymous function is called on {} context with no parameters
+ */
+{
+    (function() {}).call({})
+}
+
+
+/**
+ * @number 9.
+ * @level Average
+ * @topic Tricks / comma operator
+ * @description What will be printed in console.
+ * @answer 2. Operator comma returns its last operand so the code is equal to [1,2,3][1]
+ * 
+ */
+{
+    console.log([1, 2, 3][3, 2, 1])
+}
+
+/**
+ * @number 10.
+ * @level Simple / Average
+ * @topic Variables scopes. Emulating private members.
+ * @description How to change the code so the count can be changed only inside Foo class
+ * @answer
+ *     'this.count = 0' changed to var 'count = 0' and all its using accordingly.
+ */
+{
+
+    function Foo() {
+        this.count = 0;
+        this.someMethod = function() {
+            this.count++;
+        };
+        this.getCount = function() {
+            return this.count;
+        }
+    }
+
+    var bar = new Foo();
+    bar.someMethod();
+    //...
+    // bar.count = 100; should not be allowed
+}
+
+/**
+ * @number 11.
+ * @level Simple
+ * @topic Prototypes
+ * @description Change the code so every instance of Foo has addCount function
+ * @answer
+ *     Correct but bad way add something like this.addCount = addCount to the constructor
+ *     Good answer: Foo.prototype.addCount = function(){/* *\/}
+ */
+
+{
+    var count = 0
+
+    function addCount() {
+        count++;
+    }
+
+    function Foo() {}
+
+    var bar = new Foo();
+    var buz = new Foo();
+
+    //bar.addCount(); should be allowed
+    //buz.addCount(); should be allowed
+}
+
+
+/**
+ * @number 12.
+ * @level Simple / Average
+ * @topic Variable scopes
+ * @description What will be printed in console.
+ * @answer 0.
+ * @additional
+ *     What will be the output if foo returns i instead of bar
+ *     Answer: -1
+ */
+
+{
+    function foo() {
+        
+        for (var i = 5; i >= 0; i--) {
+            var bar = i;
+            console.log(i);
+        };
+        return bar;
+    }
+
+    console.log(foo());
+}
+
+
+
+
+/**
  * @propositions:
- *      Closures.
+ 
  *      Function hoisting
  *
  *     Add your proposition here.
  *
- *
+ *  
  *
  * 
  */
